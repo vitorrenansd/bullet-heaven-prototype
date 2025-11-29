cd630f02037dd4a6203dd1f61fca9eb88b2e1709
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-29 18:33:25 -0300

    Começo da classe WaveManager com a logica de ondas

---
+++ scripts/WaveManager.gd
@@ -0,0 +1,7 @@
+class_name WaveManager
+extends Node
+
+@export var current_wave: int = 1
+@export var max_wave: int = 20
+@export var enemies_per_wave: int = 3
+@export var can_start_new_wave = true
