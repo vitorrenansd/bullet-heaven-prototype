58bdcae46283b36ba4f9cb306eaf8a7779482be4
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-11-29 18:31:05 -0300

    Buff na velocidade do Warrior

--- scripts/player/classes/Warrior.gd
+++ scripts/player/classes/Warrior.gd
@@ -1,8 +1,7 @@
 class_name Warrior
 extends Resource
 
-## Futuramente podem ser criadas outras classes para outros bonecos (mago, tanker, etc)
 var health: int = 100
 var damage: float = 10.0
 var attack_speed: float = 1.0
-var move_speed: float = 200.0
+var move_speed: float = 250.0
