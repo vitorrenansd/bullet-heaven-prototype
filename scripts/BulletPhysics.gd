f035e655266a313ec3cb07f45cb57c288605ce53
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-12-07 17:21:51 -0300

    Criado a classe BulletPhysics com toda a logica de projetil

---
+++ scripts/BulletPhysics.gd
@@ -0,0 +1,19 @@
+class_name BulletPhysics
+extends Area2D
+
+var travelled_distance = 0
+const SPEED = 1000
+const MAX_RANGE = 1200
+
+
+func _physics_process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta
+	travelled_distance += SPEED * delta
+	if travelled_distance > MAX_RANGE:
+		queue_free()
+
+func _on_body_entered(body):
+	queue_free()
+	if body.has_method("take_damage"):
+		body.take_damage(5.0)
