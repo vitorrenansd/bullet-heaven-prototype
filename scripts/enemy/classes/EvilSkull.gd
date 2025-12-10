384a7d952f0afb59adf4da7ad56668cdfb2d4408
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-12-10 09:08:08 -0300

    refactor: mudado de int para float o valor de HP nas classes base

--- scripts/enemy/classes/EvilSkull.gd
+++ scripts/enemy/classes/EvilSkull.gd
@@ -1,7 +1,6 @@
 class_name EvilSkull
 extends Resource
 
-## Futuramente podem ser criadas outras classes para outros inimigos
-var health: int = 10
+var health: float = 10
 var damage: float = 10.0
 var move_speed: float = 200
--- scripts/player/classes/Warrior.gd
+++ scripts/player/classes/Warrior.gd
@@ -1,7 +1,7 @@
 class_name Warrior
 extends Resource
 
-var health: int = 100
+var health: float = 100
 var damage: float = 10.0
 var attack_speed: float = 1.0
 var move_speed: float = 250.0
