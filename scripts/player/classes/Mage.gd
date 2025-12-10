aef422a899d25b9e21c902b5fcb11b5c578a9339
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-12-10 09:09:20 -0300

    buff: attack_speed base do Mage mudado de 1.0 para 1.5

--- scripts/player/classes/Mage.gd
+++ scripts/player/classes/Mage.gd
@@ -3,6 +3,6 @@ extends Resource
 
 var health: float = 65
 var damage: float = 14.0
-var attack_speed: float = 1.0
+var attack_speed: float = 1.5
 var move_speed: float = 320.0
 var health_regen: int = 1
